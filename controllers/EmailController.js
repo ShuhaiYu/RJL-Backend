047ed7c15fc125e703cc8b78023161c9f98b1a24
EdgarTang <tang@veiam.net>
2025-03-01 20:55:55 +1100

    demo: unknow task

--- controllers/EmailController.js
+++ controllers/EmailController.js
@@ -333,7 +333,7 @@ module.exports = {
             repeat_frequency: repeatFrequencyStr,
             next_reminder: null,
             type: t.name,
-            status: "INCOMPLETE",
+            status: "UNKNOWN",
             email_id: null, // 之后更新
             agency_id: agency.id, // 新增
           });
